0eba177ca3cfa38a961a397ce6c70b1a3882b43f
Y_RyuZU <ryuzu1006@gmail.com>
2023-09-22 01:24:08 +0900

    fix: JOINメッセージの修正

--- JavaBE/src/main/java/com/github/tornado2023team5/kanjichan/controller/JoinController.java
+++ JavaBE/src/main/java/com/github/tornado2023team5/kanjichan/controller/JoinController.java
@@ -24,10 +24,8 @@ public class JoinController {
                                        はじめましてウサ！moonと言うウサ！
                                        こちらは友達との予定を円滑に決めることができるサービスウサ！！🥕
                                                                               
-                                       まずはhttps://liff.line.me/2000328519-0KKJlMZ7
+                                       まずはhttps://line.me/R/ti/p/%40207ingmp
                                        から、自分の予定を登録するウサ！🗓️
-                                       https://moon-usa.vercel.app/auth?openExternalBrowser=1
-                                       からgoogleカレンダーと連携して自動で予定を反映することもできるウサよ！🌕
                                                                               
                                        使い方を知りたい時はメニューから「使い方」のボタンを押して欲しいウサ🐰
                                                                               
